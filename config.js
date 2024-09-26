41d3d819ed286afbd6cb117bab2bf7ae010c09dd
Miguel <69220866+Duck1201@users.noreply.github.com>
2024-09-26 04:05:37 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     app: {
         token: process.env.DISCORD_TOKEN || 'xxx',
-        playing: 'by the Community ❤️',
+        playing: 'Roblox',
         global: true,
         guild: process.env.GUILD_ID || 'xxx',
         extraMessages: false,
         loopMessage: false,
-        lang: 'en',
+        lang: 'pt',
         enableEmojis: false,
     },
 
